a66cf80705c9cf796ed13e2b234ee6b2fc5d6f40
Marco Aigner <m.f.aigner@googlemail.com>
2024-09-23 11:23:17 +0200

    combined word_counts & languages to "add metadata" script

---
+++ chapters/notebooks/3_2_dataset preparation/3_2_1_preprocessing/add_metadata.py
@@ -0,0 +1,49 @@
+# %%
+
+
+# Add length, sentence, and word count for titles and abstracts
+# %%
+# THIS SCRIPT CALCULATES THE LENGTH,
+# SENTENCE, AND WORD COUNT FOR TITLES AND ABSTRACTS
+import pandas as pd
+import polars as pl
+from src.util import detect_language
+from src import data
+import os
+
+DIRECTORY = '../../../../data/datasets/03_pubmed'  # CHANGE AS NEEDED
+
+files = [file for file in os.listdir(DIRECTORY) if file.endswith('.csv')]
+
+datasets = data.dict_from_directory(DIRECTORY, separator=',')
+# %%
+for subject, dataset in datasets.items():
+
+    # add vocabulary counts to dataframe
+    dataset = data.count_vocabulary(
+        dataframe=dataset,
+        columns=['title', 'abstract']
+    )
+
+    # add language of title & abstract to dataframe
+    # requires polars
+    dataset = pl.DataFrame(dataset).with_columns(
+        pl.col('title').map_elements(
+            lambda x: detect_language(x),
+            return_dtype=pl.String
+        ).alias('language_title'),
+        pl.col('abstract').map_elements(
+            lambda x: detect_language(x),
+            return_dtype=pl.String
+        ).alias('language_abstract')
+    )
+
+    # override the dataset in the collection
+    datasets[subject] = dataset
+
+# %%
+for index, (subject, dataset) in enumerate(datasets.items()):
+    dataset.write_csv(
+        f'{DIRECTORY}/{files[index]}')
+
+# %%
